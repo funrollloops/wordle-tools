40493794b5e1b20a2c5b91f7d835baaf95c64666
Sagar Mittal <sagar@sagarmittal.com>
2022-07-30 01:49:39 -0700

    Re-implement matches.py in typescript

---
+++ web/match.ts
@@ -0,0 +1,149 @@
+import { WORDS } from "./words";
+
+function assert(b: boolean, msg: string) {
+  if (!b) {
+    throw msg;
+  }
+}
+
+const HINT_GREEN = 2;
+const HINT_YELLOW = 1;
+const HINT_GRAY = 0;
+const CHAR_CODE_A = "a".charCodeAt(0);
+
+class Matcher {
+  min = new Uint8Array(26);
+  max = new Uint8Array(26);
+  // One 32-bit int per position; each entry is a bitset representing the set
+  // of disallowed characters.
+  disallowed_at = new Uint32Array(5);
+  local_min = new Uint8Array(26);
+  local_saw_gray = new Uint32Array(1);
+
+  constructor() {
+    this.max.fill(5);
+  }
+
+  update(this: Matcher, word: string, result: Uint8Array) {
+    assert(word.length == 5 && result.length == 5, "inputs have wrong len");
+    this.local_min.fill(0);
+    this.local_saw_gray.fill(0);
+    for (var i = 0; i < 5; ++i) {
+      const chr = word.charCodeAt(i) - CHAR_CODE_A;
+      assert(chr >= 0 && chr < 26, "word has invalid chars");
+      if (result[i] == HINT_GREEN) {
+        assert(
+          (this.disallowed_at[i] & (1 << chr)) == 0,
+          "green hint but position disallowed"
+        );
+        this.disallowed_at[i] = ~(1 << chr);
+        this.local_min[chr] += 1;
+      } else if (result[i] === HINT_YELLOW) {
+        this.disallowed_at[i] |= 1 << chr;
+        this.local_min[chr] += 1;
+      } else {
+        assert(result[i] == HINT_GRAY, "bad hint " + result[i]);
+        this.local_saw_gray[0] |= 1 << chr;
+      }
+    }
+    for (var chr = 0; chr < 26; ++chr) {
+      if (this.local_saw_gray[0] & (1 << chr)) {
+        assert(this.min[chr] <= this.local_min[chr], "min increased?");
+        this.max[chr] = this.local_min[chr];
+      }
+      if (this.min[chr] < this.local_min[chr])
+        this.min[chr] = this.local_min[chr];
+    }
+  }
+
+  match(this: Matcher, word: string): boolean {
+    assert(word.length == 5, "word must have len 5");
+    var local_count = this.local_min;
+    local_count.fill(0);
+    for (var i = 0; i < 5; ++i) {
+      const chr = word.charCodeAt(i) - CHAR_CODE_A;
+      if (this.disallowed_at[i] & (1 << chr)) return false;
+      local_count[chr] += 1; // Count occurrences of character.
+    }
+    // Check non-positional character count constraints.
+    for (var chr = 0; chr < 26; ++chr)
+      if (local_count[chr] < this.min[chr] || local_count[chr] > this.max[chr])
+        return false;
+    return true;
+  }
+}
+
+function print_annotated_guess(guess: string, result: Uint8Array) {
+  var s = "\x1b[1;38;2;255;255;255m";
+  for (var i = 0; i < 5; ++i) {
+    if (result[i] == HINT_GREEN) s += "\x1b[48;2;106;170;100m";
+    else if (result[i] == HINT_YELLOW) s += "\x1b[48;2;201;180;88m";
+    else s += "\x1b[48;2;120;124;126m";
+    s += " " + guess[i] + " ";
+  }
+  console.log(s + "\x1b[0m");
+}
+
+function print_matches(words: string, matcher: Matcher, limit: boolean = true) {
+  var num_matches = 0;
+  var line = "";
+  for (var i = 0; i < words.length; i += 5) {
+    const word = words.substring(i, i + 5);
+    if (!matcher.match(word)) continue;
+    num_matches += 1;
+    if (!limit || num_matches <= 13 * 5) {
+      line += word;
+      if (num_matches % 13 == 0) {
+        console.log(line);
+        line = "";
+      } else {
+        line += " ";
+      }
+    } else if (num_matches == 13 * 5 + 1) {
+      console.log("…");
+    }
+  }
+  if (line.length > 0) {
+    console.log(line);
+  }
+  console.log(num_matches, "matches");
+}
+
+function generate_hint(guess: string, answer: string): Uint8Array {
+  assert(guess.length == 5 && answer.length == 5, "incorrect length");
+  var hint = new Uint8Array(5);
+  var ans_count = new Uint8Array(26);
+  for (var i = 0; i < 5; ++i) {
+    const code = answer.charCodeAt(i);
+    if (guess.charCodeAt(i) == code) hint[i] = HINT_GREEN;
+    ans_count[code - CHAR_CODE_A]++;
+  }
+  for (var i = 0; i < 5; ++i) {
+    const chr = guess.charCodeAt(i) - CHAR_CODE_A;
+    if (hint[i] != HINT_GREEN && ans_count[chr] > 0) {
+      hint[i] = HINT_YELLOW;
+      ans_count[chr]--;
+    }
+  }
+  return hint;
+}
+
+function replay_with_answer(guesses: Array<string>) {
+  var m = new Matcher();
+  var answer = guesses[guesses.length - 1];
+  guesses.slice(0, -1).forEach((guess) => {
+    const hint = generate_hint(guess, answer);
+    print_annotated_guess(guess, hint);
+    m.update(guess, hint);
+    print_matches(WORDS, m);
+    console.log();
+  });
+  print_annotated_guess(answer, generate_hint(answer, answer));
+  console.log();
+}
+
+function main(args: string[]) {
+  replay_with_answer(args);
+}
+
+main(process.argv.slice(2));
